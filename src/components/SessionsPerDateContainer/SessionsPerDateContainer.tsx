20cdb38046b3ceb9127d8c370a637ea8a1c7c7fc
FFrancoR <franrache@gmail.com>
2025-09-04 11:54:02 -0300

    feat: building session card and adding timeframes constant

--- src/components/SessionsPerDateContainer/SessionsPerDateContainer.tsx
+++ src/components/SessionsPerDateContainer/SessionsPerDateContainer.tsx
@@ -1,7 +1,6 @@
 import type { TherapySession } from "../../types/TherapySession";
+import { DateComponent, SessionCard } from "../index.ts";
 import "./SessionsPerDateContainer.css";
-import {DateComponent} from "../DateComponent/DateComponent.tsx";
-import { SessionCard } from "../SessionCard/SessionCard.tsx";
 
 interface SessionsPerDateContainerProps {
   date: Date;
@@ -12,16 +11,17 @@ export const SessionsPerDateContainer: React.FC<
   SessionsPerDateContainerProps
 > = ({ date, sessions }) => {
   return (
-    // <section className="sessions-per-date-container">
-
-    // </section>
     <div>
       <DateComponent date={date} />
       {sessions.map((s) => (
-        <div key={s.id}>{s.patientName} </div>
+        <SessionCard
+          key={s.id}
+          patientName={s.patientName.toString()}
+          startTime={s.startDate.toLocaleTimeString()}
+          endTime={s.endDate.toLocaleTimeString()}
+          idSession={s.id.toString()}
+        />
       ))}
-      <DateComponent date={new Date()}/>
-      <SessionCard patientName="John Doe" startTime="10:00 AM" endTime="11:00 AM" idSession="1"/>
     </div>
   );
 };
--- src/service/sessionService.ts
+++ src/service/sessionService.ts
@@ -1,24 +1,27 @@
 import type { TherapySession } from "../types/TherapySession";
+import { ONE_HOUR } from "../utils/constants/Timeframes";
 
 export const sessionService = {
   async getSessionsByTherapist(idTherapist: number): Promise<TherapySession[]> {
+    const now = new Date();
+
     return [
       {
         id: 1,
-        startDate: new Date(),
-        endDate: new Date(),
+        startDate: now,
+        endDate: new Date(now.getTime() + ONE_HOUR),
         patientName: "Juan Gomez",
       },
       {
         id: 2,
-        startDate: new Date(),
-        endDate: new Date(),
+        startDate: now,
+        endDate: new Date(now.getTime() + ONE_HOUR),
         patientName: "John Doe",
       },
       {
         id: 3,
-        startDate: new Date(),
-        endDate: new Date(),
+        startDate: now,
+        endDate: new Date(now.getTime() + ONE_HOUR),
         patientName: "Pepe Perez",
       },
     ];
