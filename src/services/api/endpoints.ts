afcf7686a2729f8f7e46f98da01d0748a5bdc3db
FFrancoR <franrache@gmail.com>
2025-09-21 19:37:23 -0300

    feat: refactoring endpoints file

--- src/services/api/endpoints.ts
+++ src/services/api/endpoints.ts
@@ -1,23 +1,21 @@
-import { BASE_URL } from "./client";
-
 export const API_ENDPOINTS = {
   //AUTH
-  //   LOGIN: `${BASE_URL}/auth/login`,
-  //   REGISTER: `${BASE_URL}/auth/register`,
-  //   LOG_OUT: `${BASE_URL}/auth/logout`,
+  LOGIN: `/api/v1/auth/login`,
+  REGISTER: `/api/v1/auth/register`,
+  LOG_OUT: `/api/v1/auth/logout`,
 
   //THERAPISTS
-  //   THERAPISTS: `${BASE_URL}/api/v1/therapists`,
-  //   THERAPIST_BY_ID: (id: number) => `${BASE_URL}/therapists/${id}`,
+  GET_THERAPISTS: `/api/v1/therapist`,
+  GET_THERAPIST_BY_ID: (id: number) => `/api/v1/therapist/${id}`,
 
-  //SESSIONS
-  //   SESSIONS: `${BASE_URL}/sessions`,
-  //   SESSIONS_BY_THERAPIST: (idTherapist: number) =>
-  //     `${BASE_URL}/therapists/${idTherapist}/sessions`,
-  //   SESSIONS_BY_DATE: (idTherapist: number, date: string) =>
-  //     `${BASE_URL}/therapists/${idTherapist}/sessions?date=${date}`,
+  // SESSIONS
+  GET_ALL_SESSIONS: `/sessions`,
+  GET_SESSIONS_BY_THERAPIST: (idTherapist: number) =>
+    `/api/v1/therapists/${idTherapist}/session`,
+  GET_SESSIONS_BY_DATE: (idTherapist: number, date: string) =>
+    `/api/v1/therapists/${idTherapist}/session?date=${date}`,
 
   //PATIENTS
-  GET_ALL_ACTIVE_PATIENTS: `${BASE_URL}/api/v1/patient/findAll`,
-  GET_PATIENT_BY_ID: (id: number) => `${BASE_URL}/api/v1/patient/${id}`,
+  GET_ALL_ACTIVE_PATIENTS: `/api/v1/patient/findAll`,
+  GET_PATIENT_BY_ID: (id: number) => `/api/v1/patient/${id}`,
 } as const;
