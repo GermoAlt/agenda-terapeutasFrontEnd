c08ad2744f35889a5a0bd58bc2a5581d5c9a62c6
FFrancoR <franrache@gmail.com>
2025-09-21 21:41:27 -0300

    refactor: initializing navbar refactor

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,20 +2,10 @@ import type React from "react";
 import "./NavBar.css";
 import { AlignJustify } from "lucide-react";
 import { InputSelect } from "../InputSelect/InputSelect";
-
-interface AnchorItem {
-  label: string;
-  href: string;
-}
+import companyLogo from "../../assets/TASC-company-logo.svg";
 
 export const NavBar: React.FC = () => {
-  const anchorItems: AnchorItem[] = [
-    { label: "Home", href: "/" },
-    { label: "Sessions", href: "/sessions" },
-    { label: "About", href: "/about-us" },
-    { label: "Contact", href: "contact" },
-  ];
-
+  
   const PERIODICITY_ITEMS = [
     { text: "Daily", value: "daily" },
     { text: "Weekly", value: "weekly" },
@@ -27,23 +17,46 @@ export const NavBar: React.FC = () => {
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-content">
+          <div className="logo-container">
+            <img src={companyLogo} alt="Logo" className="logo" />
+          </div>
           <div className="mobile-nav">
             <AlignJustify />
           </div>
-          <div className="desktop-nav">
-            {anchorItems.map((item) => (
-              <a className="nav-link" href={item.href} key={item.label}>
-                {item.label}
-              </a>
-            ))}
-          </div>
+          <div className="desktop-nav"></div>
           <InputSelect
             items={PERIODICITY_ITEMS}
             initialValue={{ text: "Periodicity", value: "" }}
             onChange={(value) => console.log(value)}
           />
+          <InputSelect
+            items={PERIODICITY_ITEMS}
+            initialValue={{ text: "Periodicity", value: "" }}
+            onChange={(value) => console.log(value)}
+          />
+          <div className="avatar-placeholder"></div>
         </div>
       </div>
     </nav>
   );
 };
+
+{
+  /* {anchorItems.map((item) => (
+              <a className="nav-link" href={item.href} key={item.label}>
+                {item.label}
+              </a>
+            ))} */
+}
+
+// const anchorItems: AnchorItem[] = [
+  //   { label: "Home", href: "/" },
+  //   { label: "Sessions", href: "/sessions" },
+  //   { label: "About", href: "/about-us" },
+  //   { label: "Contact", href: "contact" },
+  // ];
+
+  // interface AnchorItem {
+//   label: string;
+//   href: string;
+// }
